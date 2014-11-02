a64da044de18a8a80492738e05ae273a858891bb
wautersj <info@wautersj.be>
2014-11-02 17:41:15 +0100

    dist

--- dist/threesixty.js
+++ dist/threesixty.js
@@ -172,6 +172,8 @@ Threesixty.prototype.load = function(options){
   var rowsCount = this.renderMeta.rows;
   var perRow = Math.floor(sources.length / rowsCount);
 
+  this.renderMeta.perRow = perRow;
+
   for (var i = 0; i < this.renderMeta.rows; i++) {
     this.frames.push([]);
     for (var j = perRow - 1; j >= 0; j--) {
@@ -280,7 +282,7 @@ Threesixty.prototype.loadRow = function(options){
           that._context.drawImage(this, 0, 0, normal.width, normal.height, 0, 0, that._canvas.width, that._canvas.height);
 
           if(currentLoad <= endIndex) {
-            that._context.globalAlpha = 0.5;
+            that._context.globalAlpha = 0.8;
 
             that._context.beginPath();
             that._context.rect(0,0,that._canvas.width,that._canvas.height);
@@ -342,38 +344,162 @@ Threesixty.prototype.getRow = function(index){
 
 
 Threesixty.prototype.renderer = function(){
-  //TODO
-  //unbind previous listeners (due to re-initiate)
-
-  var currentFrame = 0;
-
-  console.log('renderer init');
-
-  console.log(this.frames);
-  console.log(this.renderMeta);
-
   var that = this;
-  var testFrame = 0;
-  var testRow = 4;
-  var handler = setInterval(function(){
+  var meta = that.renderMeta;
+  var normal = meta.normal;
+  var HD = meta.HD;
 
-    if(testFrame>29){
-      testFrame = 0;
+  meta.currentFrame = 0;
+  meta.currentRow = 0;
+  meta.interactions = {
+    isDragging: false,
+    startFrame: null,
+    startRow: null,
+    dragPosition: null,
+    targetFrame: null,
+    targetRow: null
+  }
+
+  this.findFrame = function(row, frame) {
+    if(row!==meta.currentRow || frame!==meta.currentFrame){
+      meta.currentRow = row;
+      meta.currentFrame = frame;
+
+      var img = that.frames[row][frame];
+      window.requestAnimationFrame(function(){
+        that._context.clearRect( 0, 0, that._canvas.width, that._canvas.height);
+        that._context.drawImage(img, 0, 0, normal.width, normal.height, 0, 0, that._canvas.width, that._canvas.height);
+      });
+    }
+  }
+
+  /*
+  this.framer = function(){
+    window.requestAnimationFrame(function(){
+
+      var frame = meta.interactions.targetFrame - meta.currentFrame;
+
+      //console.log(frame);
+
+
+      that.framer();
+    });
+  }
+
+  that.framer();
+  */
+
+  var enableInteraction = function(){
+    //mouse down
+    that.$el.mousedown(function(e) {
+      down({x: e.pageX, y: e.pageY});
+    });
+
+    //mouse move
+    $(window).mousemove(function(e) {
+      if(meta.interactions.isDragging==true){
+        move({x: e.pageX, y: e.pageY});
+      }
+    });
+
+    //mouse up
+    $(window).mouseup(up);
+
+    //touch start
+    that.$el.bind('touchstart', function(e) {
+      e.preventDefault();
+      down({
+        x: e.originalEvent.touches[0].pageX,
+        y: e.originalEvent.touches[0].pageY
+      });
+    });
+
+    //touch move
+    that.$el.bind('touchmove', function(e) {
+      e.preventDefault();
+      if(meta.interactions.isDragging==true){
+        move({
+          x: e.originalEvent.touches[0].pageX,
+          y: e.originalEvent.touches[0].pageY
+        });
+      }
+    });
+
+    //touch end
+    $(window).bind('touchend', function(e) {
+      e.preventDefault();
+      up(e);
+    });
+
+    //on interaction down
+    function down(param) {
+      meta.interactions.dragPosition = param;
+      meta.interactions.startFrame = meta.currentFrame;
+      meta.interactions.startRow = meta.currentRow;
+      meta.interactions.isDragging = true;
     }
 
-    if(testRow>9){
-      testRow = 0;
+    //on interaction move
+    function move(param) {
+      var newRow = meta.currentRow;
+
+      if(that.ready==2 && that.frames.length>1){
+        //calculate offset in rows and track infinitife rotation on y
+        var diffY = Math.round((param.y - meta.interactions.dragPosition.y)/60)*-1;
+        newRow = meta.interactions.startRow + diffY;
+        var extraYRotations = newRow/that.frames.length;
+
+        //round extraYRotations to correct side
+        if(extraYRotations>0){
+          extraYRotations = Math.floor(extraYRotations);
+        } else {
+          extraYRotations = Math.floor(extraYRotations);
+        }
+
+        //fix infinitife rotation gap
+        newRow += that.frames.length*(extraYRotations*-1);
+      }
+
+      //console.log(param.x - meta.interactions.dragPosition.x);
+
+
+      //calculate offset in frames and track infinitife rotation on x
+      var diffX = Math.round((param.x - meta.interactions.dragPosition.x)/30);
+      var newFrame = meta.interactions.startFrame + diffX;
+      var extraXRotations = newFrame/meta.perRow;
+
+      //round extraXRotations to correct side
+      if(extraXRotations>0){
+        extraXRotations = Math.floor(extraXRotations);
+      } else {
+        extraXRotations = Math.floor(extraXRotations);
+      }
+
+      //fix infinitife rotation gap
+      newFrame += meta.perRow*(extraXRotations*-1);
+
+      //meta.interactions.targetFrame = newFrame;
+      //meta.interactions.targetRow = newRow;
+      that.findFrame(newRow, newFrame);
     }
 
-    var frame = that.frames[testRow][testFrame];
-    if(frame && frame!==null){
-      that._context.clearRect( 0, 0, that._canvas.width, that._canvas.height );
-      that._context.drawImage(frame, 0, 0, 640, 480, 0, 0, 1280, 960)
+    //on interaction up
+    function up(param) {
+      meta.interactions.isDragging = false;
     }
+  }
 
-    testFrame ++;
-    //testRow ++;
-  }, 60);
+  // Animate the element's value from 0% to 110%:
+  $({introFrame: 0}).animate({introFrame: meta.perRow}, {
+    duration: 1500,
+    easing:'easeOutCubic', // can be anything
+    step: function() { // called on every step
+      that.findFrame(that.renderMeta.startRow,Math.floor(this.introFrame));
+    },
+    complete: function(){
+      enableInteraction();
+    }
+  });
 }
 
 
